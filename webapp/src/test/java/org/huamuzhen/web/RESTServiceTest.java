6457d544bbbcfbb1cefc5944d4f0ec5e44078b0f
lliu161 <lliu161@ODC-China-L3.homeoffice.Wal-Mart.com>
2012-06-19 11:54:41 -0700

    add test

--- webapp/src/test/java/org/huamuzhen/web/RESTServiceTest.java
+++ webapp/src/test/java/org/huamuzhen/web/RESTServiceTest.java
@@ -1,17 +1,29 @@
 package org.huamuzhen.web;
 
+import java.util.Collections;
+
+import javax.ws.rs.core.Response;
+
+import org.apache.cxf.jaxrs.client.ResponseReader;
 import org.apache.cxf.jaxrs.client.WebClient;
 import org.huamuzhen.dto.ErrorDTO;
 import org.testng.annotations.Test;
 
 public class RESTServiceTest {
+	ResponseReader reader = new ResponseReader();
+	
+	
+	RESTServiceTest(){
+		reader.setEntityClass(String.class);
+	}
 	
 	@Test
 	public void test(){
-		WebClient client = WebClient.create("http://localhost:8080/huamuzhen/rest/hello");
-	/*	String data = client.path("rest/hello").type("text/plain").accept("text/plain").get(String.class);
+		WebClient client = getWebClient("http://localhost:8080/huamuzhen/rest/");
+		Response data = client.path("/hello").type("text/plain").accept("text/plain").get();
+
 		
-		JAXRSClientFactoryBean sf = new JAXRSClientFactoryBean();
+		/*JAXRSClientFactoryBean sf = new JAXRSClientFactoryBean();
 		sf.setResourceClass(HelloRESTService.class);
 		sf.setAddress("http://localhost:8080/huamuzhen/rest/");
 		BindingFactoryManager manager = sf.getBus().getExtension(BindingFactoryManager.class);
@@ -20,20 +32,33 @@ public class RESTServiceTest {
 		manager.registerBindingFactory(JAXRSBindingFactory.JAXRS_BINDING_ID, factory);
 		HelloRESTService service = sf.create(HelloRESTService.class);
 		WebClient wc = sf.createWebClient();
-		Response data = wc.path("/hello").accept("text/plain").get();
+		Response data = wc.path("/hello").accept("text/plain").get();*/
 		System.out.println(data.getStatus());
-		System.out.println(data.getEntity().toString());
-		System.out.println(data);*/
-		
-/*		Response data2 = client.path("rest/hello").type("text/plain").accept("text/plain").get();
-		System.out.println(data2.getClass());
-		System.out.println(data2.getEntity());
-		System.out.println();*/
-		
-		ErrorDTO data1 = client.path("update").type("application/json").invoke("PUT",null,ErrorDTO.class);
-		System.out.println(data1.getCode());
-		System.out.println(data1.getStatus());
+		System.out.println(data.getEntity());
 
+	}
+	
+	@Test
+	public void test2(){
+		WebClient client = getWebClient("http://localhost:8080/huamuzhen/rest/");
+		Response data = client.path("/hello/say").type("text/plain").accept("text/plain").get();
+		System.out.println(data.getStatus());
+		System.out.println(data.getEntity());
+	}
+	
+	@Test
+	public void test3(){
+		/*ResponseReader reader = new ResponseReader(ErrorDTO.class);*/
+		reader.setEntityClass(String.class);
+		WebClient client = getWebClient("http://localhost:8080/huamuzhen/rest/");
+		ErrorDTO data = client.path("/hello/add").type("application/json").accept("application/json").post(null, ErrorDTO.class);
+		/*String error = data.getEntity();
+		System.out.println(error);*/
+	}
+	
+	
+	private WebClient getWebClient(String address){
+		return WebClient.create(address,Collections.singletonList(reader));
 	}
 
 }
